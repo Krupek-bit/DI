aa9a1a3dab86703f745798eecae6c87f42461e09
Krupek <engs-gabrielkrupek@camporeal.edu.br>
2026-05-31 20:19:13 -0300

    Inserindo getters e setters

--- Classes/Pedido.java
+++ Classes/Pedido.java
@@ -6,3 +6,47 @@ public class Pedido {
     private int clienteId;
     private String status;
     private LocalDateTime dataCriacao;
+
+        public Pedido() {
+    }
+
+    public Pedido(int idPedido, int clienteId, String status, LocalDateTime dataCriacao) {
+        this.idPedido = idPedido;
+        this.clienteId = clienteId;
+        this.status = status;
+        this.dataCriacao = dataCriacao;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(int clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+}
