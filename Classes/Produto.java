24042d408481b67e379f14b6ef25563836718e12
Gustavo <engs-gustavoborges@camporeal.edu.br>
2026-05-31 19:53:20 -0300

    entidade produto

---
+++ Classes/Produto.java
@@ -0,0 +1,63 @@
+package Classes;
+
+public class Produto {
+
+    private int id;
+    private String nome;
+    private double preco;
+    private int estoque;
+    private Categoria categoria;
+
+    public Produto(String nome, double preco, int estoque, Categoria categoria) {
+        validar(nome, preco, estoque);
+
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+        this.categoria = categoria;
+    }
+
+    public Produto(int id, String nome, double preco, int estoque, Categoria categoria) {
+        validar(nome, preco, estoque);
+
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+        this.categoria = categoria;
+    }
+
+    private void validar(String nome, double preco, int estoque) {
+        if (nome == null || nome.trim().isEmpty()) {
+            throw new IllegalArgumentException("Nome inválido.");
+        }
+
+        if (preco <= 0) {
+            throw new IllegalArgumentException("Preço inválido.");
+        }
+
+        if (estoque < 0) {
+            throw new IllegalArgumentException("Estoque inválido.");
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+}
