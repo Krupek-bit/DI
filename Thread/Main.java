86382fccd9c70148bfc265629cb9702df1fa47f5
Krupek <engs-gabrielkrupek@camporeal.edu.br>
2026-06-05 15:26:45 -0300

    Continuação do Menu principal

---
+++ Thread/Main.java
@@ -0,0 +1,16 @@
+import Thread.ProcessadorPedidos;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ProcessadorPedidos processador =
+                new ProcessadorPedidos();
+
+        processador.start();
+
+        System.out.println(
+                "Sistema iniciado."
+        );
+    }
+}
