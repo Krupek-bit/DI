94174783da9c65732abbb5e6b17296ba0898f043
Krupek <engs-gabrielkrupek@camporeal.edu.br>
2026-06-05 15:21:43 -0300

    VoidPedido

--- DAO/PedidoDAO.java
+++ DAO/PedidoDAO.java
@@ -8,3 +8,31 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+public class PedidoDAO {
+
+    public void salvar(Pedido pedido) {
+
+        String sql =
+                "INSERT INTO pedido(cliente_id, status, data_criacao) VALUES (?, ?, ?)";
+
+        try (
+                Connection conn = Conexao.conectar();
+                PreparedStatement stmt = conn.prepareStatement(sql)
+        ) {
+
+            stmt.setInt(1, pedido.getClienteId());
+            stmt.setString(2, pedido.getStatus());
+            stmt.setTimestamp(
+                    3,
+                    Timestamp.valueOf(
+                            pedido.getDataCriacao()
+                    )
+            );
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
