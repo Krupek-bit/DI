00806d1f0bd58261da0f67844a94c2809a58566e
Krupek <engs-gabrielkrupek@camporeal.edu.br>
2026-06-05 14:49:46 -0300

    Public item pedido

--- Classes/ItemPedido.java
+++ Classes/ItemPedido.java
@@ -8,3 +8,20 @@ public class ItemPedido {
     private int quantidade;
     private double precoUnitario;
 
+    public ItemPedido(int pedidoId, int produtoId, int quantidade, double precoUnitario) {
+
+        if (quantidade <= 0) {
+            throw new IllegalArgumentException("Quantidade inválida.");
+        }
+
+        if (precoUnitario <= 0) {
+            throw new IllegalArgumentException("Preço inválido.");
+        }
+
+        this.pedidoId = pedidoId;
+        this.produtoId = produtoId;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+  
