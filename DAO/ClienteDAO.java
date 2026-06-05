743ee7e1fc8d57ebdc99b11d10acaafa1c03fc90
Krupek <engs-gabrielkrupek@camporeal.edu.br>
2026-06-05 15:17:19 -0300

    criando o salvar

--- DAO/ClienteDAO.java
+++ DAO/ClienteDAO.java
@@ -7,3 +7,25 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+public class ClienteDAO {
+
+    public void salvar(Cliente cliente) {
+
+        String sql =
+                "INSERT INTO cliente(nome, email) VALUES (?, ?)";
+
+        try (
+                Connection conn = Conexao.conectar();
+                PreparedStatement stmt = conn.prepareStatement(sql)
+        ) {
+
+            stmt.setString(1, cliente.getNome());
+            stmt.setString(2, cliente.getEmail());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
