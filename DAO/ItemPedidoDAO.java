b0ccab38f6c0bd73241a319d9d909f48a251eee4
Krupek <engs-gabrielkrupek@camporeal.edu.br>
2026-06-05 15:23:19 -0300

    Finalização

--- DAO/ItemPedidoDAO.java
+++ DAO/ItemPedidoDAO.java
@@ -30,4 +30,40 @@ public class ItemPedidoDAO {
             e.printStackTrace();
         }
     }
+    public List<ItemPedido> listarPorPedido(int pedidoId) {
 
+        List<ItemPedido> itens = new ArrayList<>();
+
+        String sql =
+                "SELECT * FROM item_pedido WHERE pedido_id = ?";
+
+        try (
+                Connection conn = Conexao.conectar();
+                PreparedStatement stmt = conn.prepareStatement(sql)
+        ) {
+
+            stmt.setInt(1, pedidoId);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+
+                while (rs.next()) {
+
+                    itens.add(
+                            new ItemPedido(
+                                    rs.getInt("id_item"),
+                                    rs.getInt("pedido_id"),
+                                    rs.getInt("produto_id"),
+                                    rs.getInt("quantidade"),
+                                    rs.getDouble("preco_unitario")
+                            )
+                    );
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return itens;
+    }
+}
