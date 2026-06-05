ea7d478510f43a9f1036b7cae59c6dbc27b8d9e6
Krupek <engs-gabrielkrupek@camporeal.edu.br>
2026-06-05 15:19:46 -0300

    Primeiro public

--- DAO/ProdutoDAO.java
+++ DAO/ProdutoDAO.java
@@ -8,3 +8,27 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+public class ProdutoDAO {
+
+    public void salvar(Produto produto) {
+
+        String sql =
+                "INSERT INTO produto(nome, preco, estoque, categoria) VALUES (?, ?, ?, ?)";
+
+        try (
+                Connection conn = Conexao.conectar();
+                PreparedStatement stmt = conn.prepareStatement(sql)
+        ) {
+
+            stmt.setString(1, produto.getNome());
+            stmt.setDouble(2, produto.getPreco());
+            stmt.setInt(3, produto.getEstoque());
+            stmt.setString(4, produto.getCategoria().name());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
