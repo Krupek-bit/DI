dd04d2bfe4a4501a8403ecfb6201623344c10999
Krupek <engs-gabrielkrupek@camporeal.edu.br>
2026-06-05 14:49:19 -0300

    Classe

---
+++ Classes/ItemPedido.java
@@ -0,0 +1,10 @@
+package Classes;
+
+public class ItemPedido {
+
+    private int id;
+    private int pedidoId;
+    private int produtoId;
+    private int quantidade;
+    private double precoUnitario;
+
